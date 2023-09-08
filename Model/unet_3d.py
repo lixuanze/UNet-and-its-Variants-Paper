482618d304c238ef2d0343da62579d9ab5462fbc
lixuanze <61913302+lixuanze@users.noreply.github.com>
2023-09-08 09:08:05 -0400

    Update unet_3d.py

--- Model/unet_3d.py
+++ Model/unet_3d.py
@@ -16,7 +16,7 @@ import cv2
 
 class UNet:
   """
-  Class for Deep Learning Hyperspectral Segmentation with a 3D-UNet (the baseline model, by Ronneberger et al, 2015 & Çiçek et al, 2016)
+  Class for Deep Learning Hyperspectral Segmentation with a 3D-UNet (the baseline model, similar to the one by Ronneberger et al, 2015 & Çiçek et al, 2016)
   Input:  utils: the utilility class
   """
   def __init__(self, utils):
