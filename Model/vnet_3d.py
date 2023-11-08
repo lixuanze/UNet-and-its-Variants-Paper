4fe7cd482ec56dbd7b1a1c2c0776a493bd15ee5c
lixuanze <61913302+lixuanze@users.noreply.github.com>
2023-11-08 09:51:43 -0500

    Update vnet_3d.py

--- Model/vnet_3d.py
+++ Model/vnet_3d.py
@@ -1,3 +1,5 @@
+from re import I
+import os
 from utils import utils
 import tensorflow as tf
 from tensorflow import reduce_sum
@@ -19,6 +21,7 @@ class VNet:
   """
   def __init__(self, utils):
     self.utils = utils
+    self.data_path = os.path.join(os.getcwd(), '')
   
   def encoding_layers_building_blocks(self, units, in_layer, downsampling_layer=True):
       '''
@@ -65,17 +68,14 @@ class VNet:
       return out_layer
     
   def dice_loss(self, y_true, y_pred, smooth=1e-6):
-    '''
-    The custom dice loss as defined in the paper.
-    '''
-    # Dice Loss
-    y_true_f = Flatten()(y_true)
-    y_pred_f = Flatten()(y_pred)
-    intersection = reduce_sum(y_true_f * y_pred_f)
-    dice = (2. * intersection + smooth) / (reduce_sum(y_true_f) + reduce_sum(y_pred_f) + smooth)
-    dice_loss = 1 - dice  # Corrected this line
-    
-    return dice_loss
+    # Calculate intersection and the sum for the numerator and denominator of the Dice score
+    intersection = K.sum(y_true * y_pred, axis=[0, 1, 2])
+    sum_true_pred = K.sum(y_true, axis=[0, 1, 2]) + K.sum(y_pred, axis=[0, 1, 2])
+
+    # Calculate the Dice score for each class
+    dice_scores = (2. * intersection + smooth) / (sum_true_pred + smooth)
+    dice_loss_multiclass = 1 - K.mean(dice_scores)
+    return dice_loss_multiclass
 
   def build_3d_vnet(self):
     '''
@@ -83,71 +83,71 @@ class VNet:
     '''
     input_layer = Input((self.utils.resized_x_y, self.utils.resized_x_y, self.utils.num_components_to_keep, 1))
     # down sampling blocks
-    # 64 Channels
+    # 16 Channels
     skip_connection_1 = input_layer
-    down_sampling_convolution_layer_1 = Conv3D(64, (5, 5, 5), padding='same', kernel_initializer='he_normal')(input_layer)
+    down_sampling_convolution_layer_1 = Conv3D(16, (5, 5, 5), padding='same', kernel_initializer='he_normal')(input_layer)
     down_sampling_convolution_layer_1 = BatchNormalization()(down_sampling_convolution_layer_1)
     down_sampling_convolution_layer_1 = PReLU()(down_sampling_convolution_layer_1)
     down_sampling_convolution_layer_1 = Add()([down_sampling_convolution_layer_1, skip_connection_1])
-    down_sampling_output_layer_1 = Conv3D(128, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(down_sampling_convolution_layer_1)
+    down_sampling_output_layer_1 = Conv3D(32, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(down_sampling_convolution_layer_1)
     
-    # 128 Channels
+    # 32 Channels
     skip_connection_2 = down_sampling_output_layer_1
-    down_sampling_convolution_layer_2 = Conv3D(128, (5, 5, 5), padding='same', kernel_initializer='he_normal')(down_sampling_output_layer_1)
+    down_sampling_convolution_layer_2 = Conv3D(32, (5, 5, 5), padding='same', kernel_initializer='he_normal')(down_sampling_output_layer_1)
     down_sampling_convolution_layer_2 = BatchNormalization()(down_sampling_convolution_layer_2)
     down_sampling_convolution_layer_2 = PReLU()(down_sampling_convolution_layer_2)
-    down_sampling_convolution_layer_2 = Conv3D(128, (5, 5, 5), padding='same', kernel_initializer='he_normal')(down_sampling_convolution_layer_2)
+    down_sampling_convolution_layer_2 = Conv3D(32, (5, 5, 5), padding='same', kernel_initializer='he_normal')(down_sampling_convolution_layer_2)
     down_sampling_convolution_layer_2 = BatchNormalization()(down_sampling_convolution_layer_2)
     down_sampling_convolution_layer_2 = PReLU()(down_sampling_convolution_layer_2)
     down_sampling_convolution_layer_2 = Add()([down_sampling_convolution_layer_2, skip_connection_2])
-    down_sampling_output_layer_2 = Conv3D(256, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(down_sampling_convolution_layer_2)
+    down_sampling_output_layer_2 = Conv3D(64, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(down_sampling_convolution_layer_2)
     
-    # 256 Channels
-    down_sampling_output_layer_3, down_sampling_convolution_layer_3 = self.encoding_layers_building_blocks(256, down_sampling_output_layer_2)
-    # 512 Channels
-    down_sampling_output_layer_4, down_sampling_convolution_layer_4 = self.encoding_layers_building_blocks(512, down_sampling_output_layer_3)
-    # encoding blocks, 1024 Channels
-    encoding_space_output_layer = self.encoding_layers_building_blocks(1024, down_sampling_output_layer_4, downsampling_layer=False)
+    # 64 Channels
+    down_sampling_output_layer_3, down_sampling_convolution_layer_3 = self.encoding_layers_building_blocks(64, down_sampling_output_layer_2)
+    # 128 Channels
+    down_sampling_output_layer_4, down_sampling_convolution_layer_4 = self.encoding_layers_building_blocks(128, down_sampling_output_layer_3)
+    # encoding blocks, 256 Channels
+    encoding_space_output_layer = self.encoding_layers_building_blocks(256, down_sampling_output_layer_4, downsampling_layer=False)
     
     # up sampling blocks
-    # 512 Channels
-    up_sampling_output_layer_1 = self.decoding_layers_building_blocks(512, encoding_space_output_layer, down_sampling_convolution_layer_4)
-    # 256 Channels
-    up_sampling_output_layer_2 = self.decoding_layers_building_blocks(256, up_sampling_output_layer_1, down_sampling_convolution_layer_3)
     # 128 Channels
+    up_sampling_output_layer_1 = self.decoding_layers_building_blocks(128, encoding_space_output_layer, down_sampling_convolution_layer_4)
+    # 64 Channels
+    up_sampling_output_layer_2 = self.decoding_layers_building_blocks(64, up_sampling_output_layer_1, down_sampling_convolution_layer_3)
+    # 32 Channels
     if down_sampling_convolution_layer_2.shape[3] == 1:
-        upsampling_layer_3 = Conv3DTranspose(128, (2, 2, 1), strides=(2, 2, 1), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_2)
+        upsampling_layer_3 = Conv3DTranspose(32, (2, 2, 1), strides=(2, 2, 1), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_2)
         skip_connection_up_3 = upsampling_layer_3
     else:
-        upsampling_layer_3 = Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_2)
+        upsampling_layer_3 = Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_2)
         skip_connection_up_3 = upsampling_layer_3
     upsampling_layer_3 = concatenate([upsampling_layer_3, down_sampling_convolution_layer_2], axis=4)
-    up_sampling_convolution_layer_3 = Conv3D(128, (5, 5, 5), padding='same', kernel_initializer='he_normal')(upsampling_layer_3)
+    up_sampling_convolution_layer_3 = Conv3D(32, (5, 5, 5), padding='same', kernel_initializer='he_normal')(upsampling_layer_3)
     up_sampling_convolution_layer_3 = BatchNormalization()(up_sampling_convolution_layer_3)
     up_sampling_convolution_layer_3 = PReLU()(up_sampling_convolution_layer_3)
-    up_sampling_convolution_layer_3 = Conv3D(128, (5, 5, 5), padding='same', kernel_initializer='he_normal')(up_sampling_convolution_layer_3)
+    up_sampling_convolution_layer_3 = Conv3D(32, (5, 5, 5), padding='same', kernel_initializer='he_normal')(up_sampling_convolution_layer_3)
     up_sampling_convolution_layer_3 = BatchNormalization()(up_sampling_convolution_layer_3)
     up_sampling_convolution_layer_3 = PReLU()(up_sampling_convolution_layer_3)
     up_sampling_output_layer_3 = Add()([up_sampling_convolution_layer_3, skip_connection_up_3])
     
-    # 64 Channels
+    # 16 Channels
     if down_sampling_convolution_layer_1.shape[3] == 1:
-        upsampling_layer_4 = Conv3DTranspose(64, (2, 2, 1), strides=(2, 2, 1), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_3)
+        upsampling_layer_4 = Conv3DTranspose(16, (2, 2, 1), strides=(2, 2, 1), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_3)
         skip_connection_up_4 = upsampling_layer_4
     else:
-        upsampling_layer_4 = Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_3)
+        upsampling_layer_4 = Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initializer='he_normal')(up_sampling_output_layer_3)
         skip_connection_up_4 = upsampling_layer_4
     upsampling_layer_4 = concatenate([upsampling_layer_4, down_sampling_convolution_layer_1], axis=4)
-    up_sampling_convolution_layer_4 = Conv3D(64, (5, 5, 5), padding='same', kernel_initializer='he_normal')(upsampling_layer_4)
+    up_sampling_convolution_layer_4 = Conv3D(16, (5, 5, 5), padding='same', kernel_initializer='he_normal')(upsampling_layer_4)
     up_sampling_convolution_layer_4 = BatchNormalization()(up_sampling_convolution_layer_4)
     up_sampling_convolution_layer_4 = PReLU()(up_sampling_convolution_layer_4)
     up_sampling_output_layer_4 = Add()([up_sampling_convolution_layer_4, skip_connection_up_4])
     
     # classification block
-    up_sampling_output_layer_4_shape = up_sampling_output_layer_4.shape
-    up_sampling_output_layer_4_2d_reshaped = Reshape((up_sampling_output_layer_4_shape[1], up_sampling_output_layer_4_shape[2], up_sampling_output_layer_4_shape[3] * up_sampling_output_layer_4_shape[4]))(up_sampling_output_layer_4)
-    output_layer = Conv2D(self.utils.n_features, (1, 1), activation='softmax', name='output_layer')(up_sampling_output_layer_4_2d_reshaped)
-
+    output_layer = Conv3D(1, (1, 1, 1))(up_sampling_output_layer_4)
+    output_layer = Reshape((self.utils.resized_x_y, self.utils.resized_x_y, self.utils.num_components_to_keep))(output_layer)
+    output_layer = Activation('softmax', name='output_layer')(output_layer)
+    
     model = Model(inputs=[input_layer], outputs=[output_layer])
     learning_rate_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-3, decay_steps=20000, decay_rate=0.99)
     model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate_scheduler), loss={'output_layer': self.dice_loss}, metrics=[tf.keras.metrics.MeanIoU(num_classes = self.utils.n_features)])
@@ -159,10 +159,10 @@ class VNet:
     '''
     if self.utils.pre_load_dataset == True:
       print("Loading Training & Validation Dataset...")
-      self.utils.X_train = np.load('X_train.npy').astype(np.float64)
-      self.utils.X_validation = np.load('X_validation.npy').astype(np.float64)
-      self.utils.y_train = np.load('y_train.npy').astype(np.float64)
-      self.utils.y_validation = np.load('y_validation.npy').astype(np.float64)
+      self.utils.X_train = np.load(os.path.join(self.data_path, 'Data/X_train.npy')).astype(np.float64)
+      self.utils.X_validation = np.load(os.path.join(self.data_path, 'Data/X_validation.npy')).astype(np.float64)
+      self.utils.y_train = np.load(os.path.join(self.data_path, 'Data/y_train.npy')).astype(np.float64)
+      self.utils.y_validation = np.load(os.path.join(self.data_path, 'Data/y_validation.npy')).astype(np.float64)
       print("Training and Validation Dataset Loaded")
     else:
       print("Data Loading...")
@@ -193,15 +193,15 @@ class VNet:
       print("Prepare Data for Training, Validation & Testing...")
       X_processed, y_processed = self.utils.prepare_dataset_for_training(X_normalized, y)
       X_train, X_, y_train, y_ = train_test_split(X_processed, y_processed, train_size = 1 - self.utils.test_ratio, test_size = self.utils.test_ratio, random_state=1234)
-      X_validation, X_test, y_validation, y_test = train_test_split(X_, y_, train_size = 1 - self.utils.test_ratio, test_size = self.utils.test_ratio, random_state=1234)
+      X_validation, X_test, y_validation, y_test = train_test_split(X_, y_, train_size = 1 - self.utils.test_ratio, test_size = self.utils.test_ratio, random_state=4321)
       self.utils.X_train, self.utils.X_validation, self.utils.X_test = X_train, X_validation, X_test
       self.utils.y_train, self.utils.y_validation, self.utils.y_test = y_train, y_validation, y_test
-      np.save('X_train.npy', self.utils.X_train)
-      np.save('X_validation.npy', self.utils.X_validation)
-      np.save('X_test.npy', self.utils.X_test)
-      np.save('y_train.npy', self.utils.y_train)
-      np.save('y_validation.npy', self.utils.y_validation)
-      np.save('y_test.npy', self.utils.y_test)
+      np.save(os.path.join(self.data_path, 'Data/X_train.npy'), self.utils.X_train)
+      np.save(os.path.join(self.data_path, 'Data/X_validation.npy'), self.utils.X_validation)
+      np.save(os.path.join(self.data_path, 'Data/X_test.npy'), self.utils.X_test)
+      np.save(os.path.join(self.data_path, 'Data/y_train.npy'), self.utils.y_train)
+      np.save(os.path.join(self.data_path, 'Data/y_validation.npy'), self.utils.y_validation)
+      np.save(os.path.join(self.data_path, 'Data/y_test.npy'), self.utils.y_test)
       print("Data Processing Completed")
     if self.utils.continue_training == True:
         # Custom objects, if any (you might need to define them depending on the custom loss, metrics, etc.)
@@ -216,7 +216,7 @@ class VNet:
         vnet = self.build_3d_vnet()
         vnet.summary()
     print("Training Begins...")
-    vnet.fit(x = self.utils.X_train, y = self.utils.y_train, batch_size = self.utils.batch_size, epochs=self.utils.num_epochs, validation_data=(self.utils.X_validation, self.utils.y_validation), callbacks=[tf.keras.callbacks.ModelCheckpoint("models/vnet_best_model.h5", save_best_only=True), tf.keras.callbacks.EarlyStopping(patience=200, restore_best_weights=True)])
+    vnet.fit(x = self.utils.X_train, y = self.utils.y_train, batch_size = self.utils.batch_size, epochs=self.utils.num_epochs, validation_data=(self.utils.X_validation, self.utils.y_validation), callbacks=[tf.keras.callbacks.ModelCheckpoint("models/vnet_best_model.h5", save_best_only=True), tf.keras.callbacks.EarlyStopping(patience=50, restore_best_weights=True)])
     print("Training Ended, Model Saved!")
     return None
 
@@ -264,8 +264,8 @@ class VNet:
     else:
       if self.utils.pre_load_dataset == True:
         print("Loading Testing Dataset...")
-        self.utils.X_test = np.load('X_test.npy').astype(np.float64)
-        self.utils.y_test = np.load('y_test.npy').astype(np.float64)
+        self.utils.X_test = np.load(os.path.join(self.data_path, 'Data/X_test.npy')).astype(np.float64)
+        self.utils.y_test = np.load(os.path.join(self.data_path, 'Data/y_test.npy')).astype(np.float64)
         print("Testing Dataset Loaded")
       else:
         print("Testing Begins...")
